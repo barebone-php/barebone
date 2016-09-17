9ae135804d422fc1dd393f78aa28b1bfb7b5779b
Kjell Bublitz <kjell.bublitz@mediacode.de>
2016-09-18 00:11:23 +0200

    move asset sources to /app/

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,12 +1,14 @@
-'use strict';
-
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
-
-elixir.config.assetsPath = 'frontend';
+elixir.config.assetsPath = '.';
 elixir.config.publicPath = '../public';
 
+/**
+ * Elixir Tasks
+ */
 elixir(function(mix) {
-    mix.browserify('app.js');
-    mix.sass('app.scss');
+
+  mix.browserify('app.js');
+  mix.sass('app.scss');
+
 });
